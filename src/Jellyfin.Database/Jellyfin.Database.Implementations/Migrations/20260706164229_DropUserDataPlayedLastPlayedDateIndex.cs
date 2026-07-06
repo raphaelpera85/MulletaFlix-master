eaf911aa133cda29d0f57040d68dc41c33505637
raphaelpera85 <raphaelpera85@users.noreply.github.com>
2026-07-06 13:45:22 -0300

    fix user data index cleanup

---
+++ src/Jellyfin.Database/Jellyfin.Database.Implementations/Migrations/20260706164229_DropUserDataPlayedLastPlayedDateIndex.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Jellyfin.Database.Implementations.Migrations
+{
+    /// <inheritdoc />
+    public partial class DropUserDataPlayedLastPlayedDateIndex : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropIndex(
+                name: "IX_UserData_UserId_ItemId_LastPlayedDate",
+                table: "UserData");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateIndex(
+                name: "IX_UserData_UserId_ItemId_LastPlayedDate",
+                table: "UserData",
+                columns: new[] { "UserId", "ItemId", "LastPlayedDate" });
+        }
+    }
+}
